bef5dc10f9f2bdc2cb7b1ca19212cab5a117402a
Emorylar <khushikhati11@gmail.com>
2024-11-28 19:45:14 +0545

    feat: updated streaks

--- backend/base/models.py
+++ backend/base/models.py
@@ -4,8 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    streak_count = models.IntegerField(default=0)
     stats = models.JSONField(default=dict, null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student')
+    last_activity_date = models.DateField(auto_now=True)
+
+    def reset_streak(self):
+        self.streak_count = 0
+        self.save()
+
+    def increment_streak(self):
+        self.streak_count += 1
+        self.save()
+
+    def __str__(self):
+        return f"{self.name}"
     
     def __str__(self):
         return f"{self.name}"
@@ -27,6 +40,7 @@ class Chapter(models.Model):
         return f"Chapter: {self.title} (Subject: {self.subject.name})"
     
 class Notes(models.Model):
+    
     content = models.TextField()
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, related_name='notes')
 
