ad7146c88dbdef704c24190efe2853353ba6fd75
Hritik-Thapa <hritikthapa331@gmail.com>
2024-11-29 13:37:05 +0545

    feat: study planner setup

--- frontend/src/Components/StudyPlanner.jsx
+++ frontend/src/Components/StudyPlanner.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
-
+import { Alert, Spinner } from "react-bootstrap"
 const StudyPlanner = () => {
+  const [studyPlan, setStudyPlan] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetchStudyPlan();
+  }, [])
+
+  const fetchStudyPlan = async () => {
+    setLoading(true);
+    try {
+      // console.log('fecth')
+      const res = await fetch('/');
+      const response = await res.json();
+      if (response.status) {
+        setStudyPlan(response.data);
+        setError('');
+        setLoading(false);
+      } else {
+        setLoading(false)
+        setError(response.message);
+        setTimeout(() => setError(''), 3000);
+      }
+    } catch (err) {
+      setLoading(false);
+      setError('Error fetching Plans');
+      setTimeout(() => { setError('') }, [3000]);
+    }
+  }
+
   return (
     <div className="w-[80%]">
       <div>
@@ -15,7 +45,18 @@ const StudyPlanner = () => {
           <hr className="border-blue-900 w-[95%] " />
         </div>
       </div>
-      <div></div>
+      <div className="m-8">
+        {
+          loading ? (<Spinner />) : studyPlan ? (<div>
+            <p>No Study Plan Yet</p>
+            <button>Generate</button>
+          </div>) : <div>
+
+          </div>
+        }
+        {error && <Alert variant="danger">{error}</Alert>}
+
+      </div>
     </div>
   );
 };
